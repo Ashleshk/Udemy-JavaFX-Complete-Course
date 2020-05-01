7b49d649fdbc02408bb145998ef329fa81a45898
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-05-02 01:44:12 +0530

    School Management System in JavaFX
    Complete Functional Project

---
+++ SchoolMangament2/src/home/Main.java
@@ -0,0 +1,25 @@
+package home;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/Home.fxml"));
+        primaryStage.setTitle("Astroboy SchoolSystem");
+        primaryStage.getIcons().add(new Image("/home/icons/icon.png"));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
